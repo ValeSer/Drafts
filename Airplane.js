e4dfedc7f9cf73e4d2fe5c17435ba4db666908e2
Valeria Serci <balerty84@gmail.com>
2021-01-14 19:44:18 +0000

    add two array elements that are both of type object

--- Airplane.js
+++ Airplane.js
@@ -1,5 +1,11 @@
-let Airplane = {
-    availableAirplanes: []
-};
+let Airplane = {};
+  Airplane.availableAirplanes = [
+    {name: 'AeroJet',
+      fuelCapacity: 800
+      },
+    {name: 'SkyJet',
+      fuelCapacity: 500
+    }
+  ];
 
 export default Airplane;
