ee09e0ea8d7d832c13462ddca9e188720c30bcf0
Valeria Serci <balerty84@gmail.com>
2021-01-14 21:06:02 +0000

    calls the function

--- missionControl.js
+++ missionControl.js
@@ -2,6 +2,8 @@ import Airplane from './airplane';
 
 function displayFuelCapacity() {
   Airplane.availableAirplanes.forEach(function(element){
-  console.log('Fuel Capacity of' + element.name + ': ' + element.fuelCapacity);
+  console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
   });
 }
+
+displayFuelCapacity()
