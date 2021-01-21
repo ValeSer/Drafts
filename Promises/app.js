66b5ba36786d10943fd1fa551690fadcb0ae4917
Valeria Serci <balerty84@gmail.com>
2021-01-21 19:10:56 +0000

    add 3 variables who check the availability of sunglasses, pants and bags

--- Promises/app.js
+++ Promises/app.js
@@ -12,3 +12,8 @@ const onReject = (rejectionReason) => {
 // Write your code below:
 
 
+const checkSunglasses = checkAvailability('sunglasses', 'Favorite Supply Co.' );
+
+const checkPants = checkAvailability('pants', 'Favorite Supply Co.');
+
+const checkBags = checkAvailability('bags', 'Favorite Supply Co.');
