4e7efe6b81eb622a437a58bd8bd32984455bd339
Valeria Serci <balerty84@gmail.com>
2021-01-21 20:24:29 +0000

    app with new ex about asynchronous actions

--- Promises/app.js
+++ Promises/app.js
@@ -1,21 +1,34 @@
-const {checkAvailability} = require('./library.js');
+const fs = require('fs');
+const promisifiedReadfile = require('./promisifiedReadfile');
+      
+// Here we use fs.readfile() and callback functions:
+fs.readFile('./file.txt', 'utf-8', (err, data) => {
+  if (err) throw err;
+  let firstSentence = data;
+  fs.readFile('./file2.txt',  'utf-8', (err, data) => {
+    if (err) throw err;
+    let secondSentence = data;
+    console.log(firstSentence, secondSentence)
+  });
+});
 
-const onFulfill = (itemsArray) => {
-  console.log(`Items checked: ${itemsArray}`);
-  console.log(`Every item was available from the distributor. Placing order now.`);
-};
+// Here we use native promises with our "promisified" version of readfile:
+let firstSentence
+promisifiedReadfile('./file.txt', 'utf-8')
+  .then((data) => {
+    firstSentence = data;
+    return promisifiedReadfile('./file2.txt', 'utf-8')
+  })
+  .then((data) => {
+    let secondSentence = data;
+    console.log(firstSentence, secondSentence)
+  })
+  .catch((err) => {console.log(err)});
 
-const onReject = (rejectionReason) => {
-	console.log(rejectionReason);
-};
-
-// Write your code below:
-
-const checkSunglasses = checkAvailability('sunglasses', 'Favorite Supply Co.' );
-
-const checkPants = checkAvailability('pants', 'Favorite Supply Co.');
-
-const checkBags = checkAvailability('bags', 'Favorite Supply Co.');
-
-Promise.all([checkSunglasses, checkPants, checkBags]).then(onFulfill).catch(onReject);
-
+// Here we use promisifiedReadfile() again but instead of using the native promise .then() syntax, we declare and invoke an async/await function:
+async function readFiles() {
+  let firstSentence = await promisifiedReadfile('./file.txt', 'utf-8')
+  let secondSentence = await promisifiedReadfile('./file2.txt', 'utf-8')
+  console.log(firstSentence, secondSentence)
+}
+readFiles()
