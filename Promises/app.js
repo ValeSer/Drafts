02f9493d000eb7bd4ac0cb2c652ac774ebb0b7ed
Valeria Serci <balerty84@gmail.com>
2021-01-21 19:12:57 +0000

    invoke Promise.all

--- Promises/app.js
+++ Promises/app.js
@@ -17,3 +17,5 @@ const checkSunglasses = checkAvailability('sunglasses', 'Favorite Supply Co.' );
 const checkPants = checkAvailability('pants', 'Favorite Supply Co.');
 
 const checkBags = checkAvailability('bags', 'Favorite Supply Co.');
+
+Promise.all([checkSunglasses, checkPants, checkBags]);
