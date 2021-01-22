b8abf13b8a9a588c31e3d9b176bfe97371265b26
Valeria Serci <balerty84@gmail.com>
2021-01-22 22:00:15 +0000

    add a second variable

--- Promises/app.js
+++ Promises/app.js
@@ -3,4 +3,7 @@ const {shopForBeans, soakTheBeans, cookTheBeans} = require('./library.js');
 // Write your code below:
  async function makeBeans() {
     let type = await shopForBeans();
+
+    let isSoft = await soakTheBeans(type);
+ }
  }
