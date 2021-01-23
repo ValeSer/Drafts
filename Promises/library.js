3e70c63373097423389b06954786b22e71038c97
Valeria Serci <balerty84@gmail.com>
2021-01-23 22:02:47 +0000

    library ready for try...catch exercise

--- Promises/library.js
+++ Promises/library.js
@@ -1,40 +1,26 @@
-/*
-This is the shopForBeans function from the last exercise
-*/
-
-const shopForBeans = () => {
+//This function returns true 50% of the time.
+let randomSuccess = () => {
+    let num = Math.random();
+    if (num < .5 ){
+      return true;
+    } else {
+      return false;
+    }
+   };
+   
+   //This function returns a promise that resolves half of the time and rejects half of the time
+   let cookBeanSouffle = () => {
     return new Promise((resolve, reject) => {
-      const beanTypes = ['kidney', 'fava', 'pinto', 'black', 'garbanzo'];
-    setTimeout(()=>{
-      let randomIndex = Math.floor(Math.random() * 5)
-      let beanType = beanTypes[randomIndex];
-      console.log(`I bought ${beanType} beans because they were on sale.`)
-     resolve(beanType);
-    }, 1000)
-  })
-  }
-  
-  let soakTheBeans = (beanType) => {
-     return new Promise((resolve, reject) => {
-       console.log('Time to soak the beans.')
+      console.log('Fingers crossed... Putting the Bean Souffle in the oven');
       setTimeout(()=>{
-        console.log(`... The ${beanType} beans are softened.`)
-        resolve(true)
-        }, 1000)
-    })
-  }
-  
-  let cookTheBeans = (isSoftened) => {
-    return new Promise((resolve, reject) => {
-      console.log('Time to cook the beans.')
-      setTimeout(()=>{
-        if (isSoftened) {
-          console.log('... The beans are cooked!') 
-          resolve('\n\nDinner is served!')
+        let success = randomSuccess();
+        if(success){
+          resolve('Bean Souffle');
+        } else {
+          reject('Dinner is ruined!');
         }
-      }, 1000)
+      }, 1000);
     })
-  }
-  
-    
-  module.exports = {shopForBeans, soakTheBeans, cookTheBeans} 
+   };
+   
+   module.exports = cookBeanSouffle;
