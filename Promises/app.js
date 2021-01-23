81334d7d44b13d5d8df14e5fd4a2cced10d750db
Valeria Serci <balerty84@gmail.com>
2021-01-23 22:02:01 +0000

    app ready for new exercise about try...catch

--- Promises/app.js
+++ Promises/app.js
@@ -1,14 +1,8 @@
-const {shopForBeans, soakTheBeans, cookTheBeans} = require('./library.js');
+const cookBeanSouffle = require('./library.js');
 
 // Write your code below:
- async function makeBeans() {
-    let type = await shopForBeans();
 
-    let isSoft = await soakTheBeans(type);
 
-    let dinner = await cookTheBeans(isSoft);
 
-    console.log(dinner);
- }
- 
- makeBeans();
+
+
