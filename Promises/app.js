5e43a3d3254b8d07bf4cdebc55a8f0fef64711bb
Valeria Serci <balerty84@gmail.com>
2021-01-24 23:49:32 +0000

    declare a function and create 4 variables

--- Promises/app.js
+++ Promises/app.js
@@ -2,5 +2,10 @@ let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./library.js')
 
 // Write your code below:
 
-
+async function serveDinner() {
+  const vegetablePromise = steamBroccoli();
+  const starchPromise = cookRice();
+  const proteinPromise = bakeChicken();
+  const sidePromise = cookBeans();
+}
 
