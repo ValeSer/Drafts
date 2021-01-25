ac0e977c805094424c2f52c756de5b1897a3168d
Valeria Serci <balerty84@gmail.com>
2021-01-25 20:55:36 +0000

    invoke function

--- Promises/app.js
+++ Promises/app.js
@@ -6,3 +6,5 @@ async function serveDinnerAgain() {
 
     console.log(`Dinner is served. We're having ${foodArray[0]}, ${foodArray[1]}, ${foodArray[2]}, and ${foodArray[3]}.`)
 }
+
+serveDinnerAgain();
