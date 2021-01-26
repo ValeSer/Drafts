e912c354a7f4e74e65ca59c10527e93b13ebf379
Valeria Serci <balerty84@gmail.com>
2021-01-26 20:26:00 +0000

    new exercise with API

--- Promises/requests/main.js
+++ Promises/requests/main.js
@@ -1,22 +1,23 @@
-// First, we need to create the XMLHttpRequest object using the new operator. Save this object in a const called xhr.
-
-// Note: While the code will work regardless of how you name your variables, it is a common practice to name this object xhr.
-
-const xhr = new XMLHttpRequest();
-
-const url = 'https://api-to-call.com/endpoint';
-
-xhr.responseType = 'json';
-
-//The purpose of this conditional statement checks to see if the request has finished.
-
-xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-        return xhr.response
-    }
-  }
+// Information to reach API
 
 
-  xhr.open('GET', url);
+// Selecting page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
 
-  xhr.send();
+// AJAX function
+const getSuggestions = () => {
+
+}
+
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  };
+  getSuggestions();
+}
+
+submit.addEventListener('click', displaySuggestions);
