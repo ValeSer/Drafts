850a78a12b507dda7e59ccb65e627dda6b7d6c1e
Valeria Serci <balerty84@gmail.com>
2021-01-26 20:34:02 +0000

    starting body to getSuggestions

--- Promises/requests/main.js
+++ Promises/requests/main.js
@@ -1,8 +1,5 @@
 // Information to reach API
 const url = 'https://api.datamuse.com/words?'
-
-// Information to reach API
-const url = 'https://api.datamuse.com/words?'
 const queryParams = 'rel_rhy='
 // 'rel_rhy=' is the start of a parameter for the query string. This parameter will narrow your search to words that rhyme.
 // Selecting page elements
@@ -12,29 +9,8 @@ const responseField = document.querySelector('#responseField');
 
 // AJAX function
 const getSuggestions = () => {
-
-}
-
-// Clear previous results and display results to webpage
-const displaySuggestions = (event) => {
-  event.preventDefault();
-  while(responseField.firstChild){
-    responseField.removeChild(responseField.firstChild);
-  };
-  getSuggestions();
-}
-
-submit.addEventListener('click', displaySuggestions);
-
-
-// Selecting page elements
-const inputField = document.querySelector('#input');
-const submit = document.querySelector('#submit');
-const responseField = document.querySelector('#responseField');
-
-// AJAX function
-const getSuggestions = () => {
-
+  const wordQuery = inputField.value;
+  //inputField.value grabs what is in the inputField and assigns it to the variable wordQuery
 }
 
 // Clear previous results and display results to webpage
