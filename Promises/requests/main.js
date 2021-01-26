e9a1fa502bf6f1068aac024628d875c7d2759ce6
Valeria Serci <balerty84@gmail.com>
2021-01-26 19:21:07 +0000

    add an explanation

--- Promises/requests/main.js
+++ Promises/requests/main.js
@@ -3,3 +3,6 @@ setTimeout(() => {
    console.log('This message will always run last...');
 }, 0);
 console.log('Second message!');
+
+// changing seconds does not affect the order of messages but just the running time.
+// 0 seconds means it runs immediately but yet the last one
