49db44492092fe57bcb22d01e0b1102773c93c7d
Valeria Serci <balerty84@gmail.com>
2021-01-26 21:03:45 +0000

    include code inside the code block of the anonymous arrow function just created

--- Promises/requests/main.js
+++ Promises/requests/main.js
@@ -1,24 +1,25 @@
 // Information to reach API
-const url = 'https://api.datamuse.com/words?'
-const queryParams = 'rel_rhy='
-// 'rel_rhy=' is the start of a parameter for the query string. 
-//This parameter will narrow your search to words that rhyme.
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_rhy=';
 
 // Selecting page elements
 const inputField = document.querySelector('#input');
 const submit = document.querySelector('#submit');
 const responseField = document.querySelector('#responseField');
 
+
+
 // AJAX function
 const getSuggestions = () => {
   const wordQuery = inputField.value;
-  //inputField.value grabs what is in the inputField and assigns it to the variable wordQuery
   const endpoint = `${url}${queryParams}${wordQuery}`;
   const xhr = new XMLHttpRequest ();
   xhr.responseType = 'json';
-  // When data is sent back, it will be in JSON format.
-  xhr.onreadystatechange = () => {};
-
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      renderRawResponse(xhr.response)
+    }
+  };
 }
 
 // Clear previous results and display results to webpage
