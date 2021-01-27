e4f272aead9d6ed9a956454878ea8fc1efd0991a
Valeria Serci <balerty84@gmail.com>
2021-01-27 20:54:17 +0000

    add explanation

--- Promises/requests/postRequest.js
+++ Promises/requests/postRequest.js
@@ -1,5 +1,6 @@
 const xhr = new XMLHttpRequest();
 const url = 'https://api-to-call.com/endpoint';
 const data = JSON.stringify({id: '200'});
-
+// JSON.stringify() will convert a value to a JSON string. 
+// By converting the value to a string, we can then send the data to a server
 
