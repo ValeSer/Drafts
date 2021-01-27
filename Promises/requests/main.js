44e0dd7f5da5fe888d3b0816fac0915e8faed8e8
Valeria Serci <balerty84@gmail.com>
2021-01-27 19:30:11 +0000

    the correct exercise page!

--- Promises/requests/main.js
+++ Promises/requests/main.js
@@ -1,47 +1,38 @@
-// Formats response to look presentable on webpage
-const renderResponse = (res) => {
-  // handles if res is falsey
-  if(!res){
-    console.log(res.status)
-  }
-  // in case res comes back as a blank array
-  if(!res.length){
-    responseField.innerHTML = "<p>Try again!</p><p>There were no suggestions found!</p>"
-    return
-  }
+// Information to reach API
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_rhy=';
 
-  // creating an array to contain the HTML strings
-  let wordList = []
-  // looping through the response and maxxing out at 10
-  for(let i = 0; i < Math.min(res.length, 10); i++){
-    // creating a list of words
-    wordList.push(`<li>${res[i].word}</li>`)
-  }
-  // joins the array of HTML strings into one string
-  wordList = wordList.join("")
+// Selecting page elements
+const inputField = document.querySelector('#input');
+const topicField = document.querySelector('#topic');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
 
-  // manipulates responseField to render the modified response
-  responseField.innerHTML = `<p>You might be interested in:</p><ol>${wordList}</ol>`
-  return
+// AJAX function
+const getSuggestions = () => {
+  const wordQuery = inputField.value;
+  const endpoint = `${url}${queryParams}${wordQuery}`;
+  
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      renderResponse(xhr.response);
+    }
+  }
+  
+  xhr.open('GET', endpoint);
+  xhr.send();
 }
 
-// Renders response before it is modified
-const renderRawResponse = (res) => {
-  // taking the first 10 words from res
-  let trimmedResponse = res.slice(0, 10)
-  //manipulates responseField to render the unformatted response
-  responseField.innerHTML = `<text>${JSON.stringify(trimmedResponse)}</text>`
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  getSuggestions();
 }
 
-// Renders the JSON that was returned when the Promise from fetch resolves.
-const renderJsonResponse = (res) => {
-  // creating an empty object to store the JSON in key-value pairs
-  let rawJson = {}
-  for(let key in response){
-    rawJson[key] = response[key]
-  }
-  // converting JSON into a string and adding line breaks to make it easier to read
-  rawJson = JSON.stringify(rawJson).replace(/,/g, ", \n")
-  // manipulates responseField to show the returned JSON.
-  responseField.innerHTML = `<pre>${rawJson}</pre>`
-}
+submit.addEventListener('click', displaySuggestions);
