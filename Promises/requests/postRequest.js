97905f69437400155845142aa4d194d2697898f4
Valeria Serci <balerty84@gmail.com>
2021-01-27 21:04:10 +0000

    send request to server

--- Promises/requests/postRequest.js
+++ Promises/requests/postRequest.js
@@ -1,18 +1,15 @@
 const xhr = new XMLHttpRequest();
 const url = 'https://api-to-call.com/endpoint';
 const data = JSON.stringify({id: '200'});
-// JSON.stringify() will convert a value to a JSON string. 
-// By converting the value to a string, we can then send the data to a server
-
 xhr.responseType = 'json';
-
-//.onreadystatechange will contain the event handler that will be called when xhr‘s state changes.
 xhr.onreadystatechange = () => {
-    // The response property will contain the data that we’re getting back from the POST request
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-        return xhr.response 
-      }
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    return xhr.response 
+  }
 }
 
 //.open() creates a new request and the arguments passed in determine what type of request is being made and where it’s being made to
 xhr.open('POST', url);
+
+//.send() will send the request to the server.
+xhr.send(data);
