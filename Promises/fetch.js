9d3938afbd5f9b1d2b4b5a3d18fe2831e46fae68
Valeria Serci <balerty84@gmail.com>
2021-02-01 20:06:55 +0000

    delet double file

--- Promises/fetch.js
+++
@@ -1,5 +0,0 @@
-fetch('https://api-to-call.com/endpoint').then(response => {
-  if (response.ok){
-    return response.json();
-  }});
-  //then() will fire only after the promise status of fetch() has been resolved.
