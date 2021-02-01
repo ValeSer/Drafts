f2599313283d3683de12ec1d885282e1630951a5
Valeria Serci <balerty84@gmail.com>
2021-02-01 20:04:06 +0000

    add explanation

--- Promises/fetch.js
+++ Promises/fetch.js
@@ -2,3 +2,4 @@ fetch('https://api-to-call.com/endpoint').then(response => {
   if (response.ok){
     return response.json();
   }});
+  //then() will fire only after the promise status of fetch() has been resolved.
