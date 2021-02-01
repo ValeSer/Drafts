5cc7ce099ea1550c193d194aa61285edf06ba57e
Valeria Serci <balerty84@gmail.com>
2021-02-01 20:08:46 +0000

    add explanation

--- Promises/requests/fetch.js
+++ Promises/requests/fetch.js
@@ -3,3 +3,7 @@ fetch('https://api-to-call.com/endpoint').then(response => {
     return response.json();
   }});
   //then() will fire only after the promise status of fetch() has been resolved.
+  //The reason we’re testing the ok property of the 
+  //response object is that it will be a Boolean value. 
+  //If there were no errors response.ok will be true 
+  //and then your code will return response.json().
