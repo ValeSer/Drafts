c5166749be8a814724f88cf78e201399c5acbf92
Valeria Serci <balerty84@gmail.com>
2021-02-01 20:16:48 +0000

    add new parameter to .then

--- Promises/requests/fetch.js
+++ Promises/requests/fetch.js
@@ -3,11 +3,6 @@ fetch('https://api-to-call.com/endpoint').then(response => {
     return response.json();
   }
   throw new Error('Request failed!');
-  });
-  //then() will fire only after the promise status of fetch() has been resolved.
-  //The reason we’re testing the ok property of the 
-  //response object is that it will be a Boolean value. 
-  //If there were no errors response.ok will be true 
-  //and then your code will return response.json().
-
-  
+  }, networkError => {
+    console.log(networkError.message);
+  })
