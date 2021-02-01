f74eb411f99205fc867679f14b419fb0a72615dc
Valeria Serci <balerty84@gmail.com>
2021-02-01 20:11:38 +0000

    new error when response.ok is falsy

--- Promises/requests/fetch.js
+++ Promises/requests/fetch.js
@@ -1,9 +1,13 @@
 fetch('https://api-to-call.com/endpoint').then(response => {
   if (response.ok){
     return response.json();
-  }});
+  }
+  throw new Error('Request failed!');
+  });
   //then() will fire only after the promise status of fetch() has been resolved.
   //The reason we’re testing the ok property of the 
   //response object is that it will be a Boolean value. 
   //If there were no errors response.ok will be true 
   //and then your code will return response.json().
+
+  
