e23e209c72a8529dccd90406adfbd988f7657635
Valeria Serci <balerty84@gmail.com>
2021-02-01 20:24:47 +0000

    add explanation

--- Promises/requests/fetch.js
+++ Promises/requests/fetch.js
@@ -8,3 +8,7 @@ fetch('https://api-to-call.com/endpoint').then(response => {
   }).then(jsonResponse => {
     return jsonResponse;
   })
+
+  //The second .then()‘s success callback won’t run until the previous
+  // .then() method has finished running. 
+  //It will also not run if there was an error was thrown previously.
