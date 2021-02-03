8ac31247da8c528cb93588519b4ebf7e6d07ef4c
Valeria Serci <balerty84@gmail.com>
2021-02-03 22:54:19 +0000

    Inside the callback function’s code block, check the ok property of the response object inside of a conditional statement. In the code block of the conditional statement, return response.json()

--- Promises/requests/fetchPost.js
+++ Promises/requests/fetchPost.js
@@ -2,5 +2,7 @@ fetch('https://api-to-call.com/endpoint', {
   method: 'POST',
   body: JSON.stringify({id: '200'})
 }).then(response => {
-
+  if(response.ok) {
+    return response.json();
+  }
 })
