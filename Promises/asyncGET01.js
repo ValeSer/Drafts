ca9babc1499582ba94348d4f0712dfda4a90b7c3
Valeria Serci <balerty84@gmail.com>
2021-02-05 23:48:49 +0000

    add if statement

--- Promises/asyncGET01.js
+++ Promises/asyncGET01.js
@@ -1,7 +1,10 @@
 const getData = async () => {
     try {
       const response = await
-      fetch('https://api-to-call.com/endpoint')
+      fetch('https://api-to-call.com/endpoint');
+      if (response.ok) {
+        
+      }
     } catch(error) {
       console.log(error);
     }
