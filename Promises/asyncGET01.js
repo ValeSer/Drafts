d911189c1c757904e7f8fe5f998a92e838d002c6
Valeria Serci <balerty84@gmail.com>
2021-02-05 23:46:38 +0000

    add await and fetch inside the try statement

--- Promises/asyncGET01.js
+++ Promises/asyncGET01.js
@@ -1,6 +1,8 @@
 const getData = async () => {
-    try {}
-    catch(error) {
+    try {
+      const response = await
+      fetch('https://api-to-call.com/endpoint')
+    } catch(error) {
       console.log(error);
     }
   };
