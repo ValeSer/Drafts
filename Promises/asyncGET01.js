7e9e702c969036021172febf1989cf4853c6a880
Valeria Serci <balerty84@gmail.com>
2021-02-05 23:51:16 +0000

    add const jsonResponse to await response.json

--- Promises/asyncGET01.js
+++ Promises/asyncGET01.js
@@ -3,7 +3,7 @@ const getData = async () => {
       const response = await
       fetch('https://api-to-call.com/endpoint');
       if (response.ok) {
-        
+        const jsonResponse = await response.json()
       }
     } catch(error) {
       console.log(error);
