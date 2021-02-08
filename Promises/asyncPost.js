15ce673e516f5207a382c3cfb74dc33e9e6bc3cd
Valeria Serci <balerty84@gmail.com>
2021-02-08 20:48:09 +0000

    fill the empty object with properties

--- Promises/asyncPost.js
+++ Promises/asyncPost.js
@@ -1,6 +1,9 @@
 const getData = async () => {
     try {
-      const response = await fetch('https://api-to-call.com/endpoint', {});
+      const response = await fetch('https://api-to-call.com/endpoint', {
+        method: 'POST',
+        body: JSON.stringify({id: 200})
+      });
     }
     catch(error) {
       console.log(error);
