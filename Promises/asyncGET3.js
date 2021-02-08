3571bf99bd2003454d3d4b159ac53f26c6f23b0a
Valeria Serci <balerty84@gmail.com>
2021-02-08 20:17:17 +0000

    cleaned the response

--- Promises/asyncGET3.js
+++ Promises/asyncGET3.js
@@ -16,7 +16,7 @@ const getSuggestions = async () => {
       const response = await fetch(endpoint, {cache: 'no-cache'});
       if (response.ok) {
         const jsonResponse = await response.json();
-         renderRawResponse(jsonResponse);
+         renderResponse(jsonResponse);
       }
     }
     catch(error) {
